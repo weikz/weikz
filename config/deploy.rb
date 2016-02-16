b980a8001257f0d1916898a7b7c0183d555d6db5
zhangkon <zhangkon@gmail.com>
2016-02-17 01:46:00 +0800

    fix rails c on production

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,8 @@ set :repo_url, 'git@github.com:zhangkon/weikz.git'
 # set :pty, true
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :bundle_binstubs, nil
 
 
 # Default value for :linked_files is []
